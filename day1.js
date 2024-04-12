7bce87bbb5943f3d61ee2db3408b4dccbbf12c86
gaurav mandal <gauravmandal97255@gmail.com>
2024-04-12 11:01:51 +0530

    dau=y1

--- day1.js
+++ day1.js
@@ -30,3 +30,38 @@ function staircase(n) {
         console.log(stair);
     }
 }
+
+
+/** question2
+ * https://www.hackerrank.com/challenges/mini-max-sum/problem?isFullScreen=false
+ * 
+ * 
+ */
+function miniMaxSum(arr) {
+    // Write your code here
+    arr.sort((a,b)=> a-b);
+    let sumO = 0 ;
+    let sumT = 0
+    for(let i = 0 ; i<arr.length-1 ; i++){
+        sumO += arr[i];
+        }
+    for( let i =1;i< arr.length;i++ ){
+         sumT += arr[i]
+    }
+   console.log(`${sumO} ${sumT}`)
+}
+
+/**
+ * https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+ */
+function birthdayCakeCandles(candles) {
+    // Write your code here
+     const max =   Math.max(...candles)
+     let count = 0
+     candles.map((e)=>{
+         if(e === max){
+             count++;
+         }
+     })
+     return count
+}
