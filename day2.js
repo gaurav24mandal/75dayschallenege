d29f632767cf002bfcd67495b3a6fed9293f2466
gaurav mandal <gauravmandal97255@gmail.com>
2024-04-13 11:03:11 +0530

    day2

--- day2.js
+++ day2.js
@@ -15,3 +15,27 @@ function countApplesAndOranges(s, t, a, b, apples, oranges) {
     })
    console.log(`${appleAtHome.length}\n${oragangesAtHome.length}`)
 }
+
+
+/**
+ * https://www.hackerrank.com/challenges/kangaroo/problem?isFullScreen=true
+ */
+function kangaroo(x1, v1, x2, v2) {
+    // Write your code here
+       let distanceToCover = x2-x1 ;
+       let relavtiveSpeed = v1-v2 ;
+   if(v2>v1){
+       return "NO"
+   }
+   else if(distanceToCover%relavtiveSpeed != 0){
+       return "NO"
+   }
+   else{
+       const noOfJumps = distanceToCover/relavtiveSpeed;
+       if(v1*noOfJumps+x1 === v2*noOfJumps+x2){
+           return "YES"
+       }
+       return "NO"
+       }
+   }
+ 
