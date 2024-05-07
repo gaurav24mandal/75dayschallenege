0f51aeb3174afafb632edeb5b5e60bbd3fd120f5
gaurav mandal <gauravmandal97255@gmail.com>
2024-05-07 23:03:07 +0530

    day 12

---
+++ day12.js
@@ -0,0 +1,16 @@
+/**
+ * Write a program to find x to the power n (i.e. x^n). Take x and n from the user. You need to return the answer.
+
+Do this recursively.
+ */
+
+function power(x,n){
+    if(n ===1){
+        return x;
+    }
+    else{
+        return x*power(x,n-1);
+    }
+}
+
+console.log(power(2,4));
